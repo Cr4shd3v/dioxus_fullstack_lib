426f019a341c9c18e833843198ddc8c7abf943d1
crashdev <n.hack2810@gmail.com>
2026-04-01 20:06:19 +0200

    added properties to text input field

--- crates/dioxus_tabler/src/component/input/input_field.rs
+++ crates/dioxus_tabler/src/component/input/input_field.rs
@@ -17,12 +17,21 @@ pub fn TextInputField(
     #[props(into, optional)]
     placeholder: String,
     #[props(into, optional)]
+    class: String,
+    #[props(into, optional)]
     on_blur: EventHandler,
+    #[props(into, optional)]
+    on_input: EventHandler,
 ) -> Element {
     let input_id = format!("form_{name}");
     let mut label_class = "form-label".to_string();
     if required {
         label_class.push_str(" required");
+    }
+
+    let mut input_class = "form-control".to_string();
+    if !class.is_empty() {
+        input_class.push_str(&*class);
     }
 
     rsx! {
@@ -41,14 +50,17 @@ pub fn TextInputField(
             id: input_id,
             name,
             type: input_type,
-            class: "form-control",
+            class: input_class,
             required,
             placeholder,
             value,
-            oninput: move |event| value.set(event.value()),
+            oninput: move |event| {
+                value.set(event.value());
+                on_input.call(());
+            },
             onblur: move |_| {
                 on_blur.call(());
-            }
+            },
         }
     }
 }
