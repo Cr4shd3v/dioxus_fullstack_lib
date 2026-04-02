9bcfacd7df094c139b1f437f7c05379b372facb3
crashdev <n.hack2810@gmail.com>
2026-04-02 21:22:39 +0200

    added repeated input type

--- crates/dioxus_tabler/src/component/input.rs
+++ crates/dioxus_tabler/src/component/input.rs
@@ -1,5 +1,7 @@
 mod input_field;
 mod checkbox;
+mod repeated_input;
 
 pub use input_field::*;
 pub use checkbox::*;
+pub use repeated_input::*;
---
+++ crates/dioxus_tabler/src/component/input/repeated_input.rs
@@ -0,0 +1,85 @@
+use dioxus::prelude::*;
+use crate::TextInputField;
+
+#[component]
+pub fn RepeatedInputField(
+    #[props(into, optional)]
+    value: Signal<String>,
+    #[props(into, default = "col-4")]
+    container_class: String,
+    #[props(into, optional)]
+    required: bool,
+    #[props(into, optional)]
+    label: String,
+    #[props(into)]
+    name: String,
+    #[props(into, default = "password")]
+    input_type: String,
+) -> Element {
+    let repeat_label = if label.is_empty() { String::new() } else { format!("Repeat {label}") };
+    let repeat_name = format!("{name}_repeat");
+    let value1 = use_signal(|| value.read().cloned());
+    let value2 = use_signal(|| value.read().cloned());
+    let mut error = use_signal(|| "");
+
+    use_effect(move || {
+        if *value1.read() != *value2.read() {
+            error.set("Inputs do not match!");
+        } else {
+            error.set("");
+        }
+    });
+
+    rsx! {
+        div {
+            class: container_class.clone(),
+            TextInputField {
+                name,
+                input_type: input_type.clone(),
+                label,
+                required,
+                value: value1,
+                class: if !error.is_empty() {
+                    "is-invalid"
+                } else {
+                    if value1.is_empty() {
+                        ""
+                    } else {
+                        "is-valid"
+                    }
+                },
+            },
+            if !error.is_empty() {
+                div {
+                    class: "invalid-feedback",
+                    {error}
+                }
+            }
+        }
+        div {
+            class: container_class,
+            TextInputField {
+                name: repeat_name,
+                input_type,
+                label: repeat_label,
+                required,
+                value: value2,
+                class: if !error.is_empty() {
+                    "is-invalid"
+                } else {
+                    if value2.is_empty() {
+                        ""
+                    } else {
+                        "is-valid"
+                    }
+                },
+            },
+            if !error.is_empty() {
+                div {
+                    class: "invalid-feedback",
+                    {error}
+                }
+            }
+        }
+    }
+}
